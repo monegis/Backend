904fb59921046825aa4be0c314663246ff29dbf6
Jeongin <sunjeongin123@gmail.com>
2025-05-09 19:42:51 +0900

    Add files via upload

---
+++ src/main/java/hackathon/monegi/MonegiApplication.java
@@ -0,0 +1,13 @@
+package hackathon.monegi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonegiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonegiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hackathon/monegi/MonegiApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.monegi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonegiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
